a5cc6cf509322eda0b4a95b78fdff95e72e0c3fd
Chelinka <106014773+ChelinkaLatov@users.noreply.github.com>
2023-04-26 20:38:31 +0200

    the code.py file is now correct
    Changed the python code to take into account the capital letter.
    Also added the fact that spaces in names may not be replicated in the passwords.
    To take this into account, two lines will be created: one with the spaces and one with them taken out, so the name is concatenated into one.

--- 2/code.py
+++ 2/code.py
@@ -3,13 +3,15 @@ with open("wordlist.txt", "r") as file:
 	names = [line.strip() for line in file.readlines()]
 
 special_chars = punctuation
-years = [str(y) for y in range(1950,2020)]
+years = [str(y) for y in range(1950,2023)]
 
 count = 0
 with open("custom_wordlist.txt", "w") as file:
 	for name in names:
 		for special_char in special_chars:
 			for year in years:
-				file.write(name+year+special_char+"\n")
+				if " " in name:
+					file.write("".join(name.split(" ")).capitalize()+year+special_char+"\n")
+				file.write(name.capitalize()+year+special_char+"\n")
 				count += 1
 print(f"{count} lines have be written to file")
